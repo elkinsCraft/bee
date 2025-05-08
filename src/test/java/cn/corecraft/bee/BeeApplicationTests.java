4650fe5a26fb39101bc07006cf8fdcfe324745c2
elkins.qiu <627292878@qq.com>
2025-05-08 23:09:06 +0800

    springboot init
    springboot init

---
+++ src/test/java/cn/corecraft/bee/BeeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.corecraft.bee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
